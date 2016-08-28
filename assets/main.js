ac6644d6ad28888abaf3f53536474f18f6400daf
Jamie Halvorson <jamie@halvorson.co.uk>
2016-08-28 17:12:30 +0100

    Increase z-index of blocker

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,12 @@
 (function($) {
+    // $(window).load(function(){
+    //     setTimeout(function(){
+    //         $(".takeover-modal").modal({
+    //             fadeDuration: 100
+    //         });
+    //     }, 3000);
+    // });
+
     $(document).ready(function() {
         if (Cookies.get('modal_shown') == null) {
             Cookies.set('modal_shown', 'yes', { expires: 3, path: '/' });
